b654ef8358b427363c71979dffe431e9956b5f5d
Dmitrii Bulatov <bulati484@gmail.com>
2023-01-26 21:10:57 +0300

    initial commit

--- public/js/api/User.js
+++ public/js/api/User.js
@@ -4,12 +4,13 @@
  * Имеет свойство URL, равное '/user'.
  * */
 class User {
+  static URL = '/user';
   /**
    * Устанавливает текущего пользователя в
    * локальном хранилище.
    * */
   static setCurrent(user) {
-
+    localStorage.setItem(`user`, JSON.stringify(user));
   }
 
   /**
@@ -17,7 +18,7 @@ class User {
    * пользователе из локального хранилища.
    * */
   static unsetCurrent() {
-
+    localStorage.removeItem(`user`);
   }
 
   /**
@@ -25,7 +26,7 @@ class User {
    * из локального хранилища
    * */
   static current() {
-
+    return JSON.parse(localStorage.getItem(`user`))
   }
 
   /**
@@ -33,7 +34,24 @@ class User {
    * авторизованном пользователе.
    * */
   static fetch(callback) {
-
+    createRequest({
+      url: this.URL + '/current',
+      method: 'GET',
+      responseType: 'json',
+      data: this.current(),
+      callback: (err, response) => {
+        if (response && response.user) {
+          const user = {
+            id: response.user.id,
+            name: response.user.name,
+          }
+          this.setCurrent(user)
+        } else {
+          this.unsetCurrent()
+        }
+        callback(err, response);
+      }
+    })
   }
 
   /**
@@ -64,7 +82,22 @@ class User {
    * User.setCurrent.
    * */
   static register(data, callback) {
-
+    createRequest({
+      url: this.URL + '/register',
+      method: 'POST',
+      responseType: 'json',
+      data,      
+      callback: (err, response) => {
+        if (response && response.user) {
+          const user = {
+            id: response.user.id,
+            name: response.user.name,
+          }
+          this.setCurrent(user);
+        }
+        callback(err, response);
+      }
+    })
   }
 
   /**
@@ -72,6 +105,17 @@ class User {
    * выхода необходимо вызвать метод User.unsetCurrent
    * */
   static logout(callback) {
-
+    createRequest({
+      url: this.URL + '/logout',
+      method: 'POST',
+      responseType: 'json',
+      data: current(),
+      callback: (err, response) => {
+        if (response) {
+          this.unsetCurrent()
+        }
+        callback(err, response);
+      }
+    })
   }
 }
--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,40 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
-
-};
+    if (options) {
+      const xhr = new XMLHttpRequest;
+      let sendURL = options.url;
+      let formData = new FormData();
+      
+      if (options.method !== 'GET') {
+        Object.entries(options.data).forEach( ( [key, value] ) => {
+          formData.append( key, value )
+        })
+      } else {
+        formData = '';
+        sendURL += '?';
+        Object.entries(options.data).forEach( ( [key, value] ) => {
+          sendURL += `${key}=${value}&`
+        })
+        sendURL = sendURL.slice(0, -1)
+      }
+  
+      try {
+        xhr.open(options.method, sendURL);
+        xhr.send(formData);
+        options.callback(null, response);
+      }
+      catch (err) {
+        options.callback(err, null);
+      }
+      xhr.responseType = 'json';
+  
+      xhr.addEventListener('readystatechange', function() {
+        if (xhr.status === 200 && xhr.readyState === xhr.DONE) {
+          options.callback(null, xhr.response);
+        } else if (xhr.status === 201 && xhr.readyState === xhr.DONE) {
+          options.callback(null, xhr.response)
+        }
+      })
+    }
+  }
